3cb1be1c8dc10971866adc24a9ec23ece9c35986
Brandon Teich <brandonteich123@gmail.com>
2019-03-01 05:13:20 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 class PigLatinizer
   
   def piglatinize(input_str)
-    input_str.split(" ").length == 1 ? piglatinize_word(input_str) : piglatinize_sentence(input_str)
+    input_str.split(" ").length == 1 ? piglatinize_word(input) : piglatinize_sentence(input)
   end
 
    private
@@ -15,7 +15,6 @@ class PigLatinizer
       word = word + "w"
     elsif consonant?(word[0]) && consonant?(word[1]) && consonant?(word[2])
       word = word.slice(3..-1) + word.slice(0,3)
-    # word starts with 2 consonants
     elsif consonant?(word[0]) && consonant?(word[1])
       word = word.slice(2..-1) + word.slice(0,2)
     # word starts with 1 consonant
